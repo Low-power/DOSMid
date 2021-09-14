5dedbc3dad223c30b3659f81d4ab0cc7fd2cb91a
Mateusz Viste <mateusz@viste.fr>
2021-09-14 08:52:44 +0000

    bumped dates
    git-svn-id: https://svn.code.sf.net/p/dosmid/code/trunk@327 850bf26b-01f9-4b63-b2ae-8b26f6eb5263

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the DOSMid project.
- * Copyright (C) 2020 Mateusz Viste
+ * Copyright (C) 2021 Mateusz Viste
  */
 
 #define PVER "0.9.6 beta"
-#define PDATE "2014-2020"
+#define PDATE "2014-2021"
